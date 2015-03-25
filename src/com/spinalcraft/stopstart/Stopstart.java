b39a95e65df5cc4415bed145aafe6edeb62bf390
ParkerKemp <charles.kemp702@topper.wku.edu>
2015-03-25 17:02:53 -0500

    Changed command

--- src/com/spinalcraft/stopstart/Stopstart.java
+++ src/com/spinalcraft/stopstart/Stopstart.java
@@ -3,7 +3,7 @@ package com.spinalcraft.stopstart;
 import java.io.IOException;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Color;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
@@ -16,13 +16,13 @@ public final class Stopstart extends JavaPlugin{
 	@Override
 	public void onEnable(){
 		console = Bukkit.getConsoleSender();
-		console.sendMessage(Color.BLUE + "Stopstart online!");
+		console.sendMessage(ChatColor.BLUE + "Stopstart online!");
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		if(cmd.getName().equalsIgnoreCase("stopstart")){
 			
-			String scriptArgs[] = {"nohup", "server-restart", "."};
+			String scriptArgs[] = {"nhp", "server-restart", "."};
 			
 			try {
 				Runtime.getRuntime().exec(scriptArgs);
